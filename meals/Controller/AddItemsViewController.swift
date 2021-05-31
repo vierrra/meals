1f26c29d42a26bf5d105d695a2a2f9e5f67d09ee
Renato <renato.vieira@stant.com.br>
2021-05-31 20:39:37 -0300

    Added Xib

---
+++ meals/Controller/AddItemsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemsViewController.swift
+//  meals
+//
+//  Created by Renato on 5/31/21.
+//
+
+import UIKit
+
+class AddItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
